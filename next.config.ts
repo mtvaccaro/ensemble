270aca9680f8c95598495478fa7bc44a7ee6ebde
vacc <matt.t.vaccaro@gmail.com>
2025-10-23 18:00:55 -0400

    fix: disable webpack cache in production builds

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   // Force cache invalidation: 2025-01-23
 
-  webpack: (config, { isServer }) => {
-    // Cache buster
-    config.cache = {
-      ...config.cache,
-      version: '2025-01-23-v2',
-    };
+  webpack: (config, { isServer, dev }) => {
+    // Disable webpack cache in production to avoid weird build errors
+    if (!dev) {
+      config.cache = false;
+    }
 
     // Don't process mediabunny on the server side
     if (isServer) {
