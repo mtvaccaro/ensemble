3ae4f6ae7ef6c11492301b1ba4d5c7e66a3bfb75
vacc <matt.t.vaccaro@gmail.com>
2025-10-25 00:45:04 -0400

    fix: remove blue hover/focus outline from UniversalPanel
    Added outline-none and focus:outline-none classes to the main panel container
    to prevent the default browser focus ring from appearing as a blue vertical
    line when hovering or interacting with the panel.
    
    User feedback: Blue stroke appeared on panel hover
    Fix: Disabled all outline states on the panel container

--- components/canvas/universal-panel.tsx
+++ components/canvas/universal-panel.tsx
@@ -99,7 +99,7 @@ export function UniversalPanel({
   return (
     <div
       className={cn(
-        'fixed top-0 right-0 h-screen w-[420px] bg-white flex flex-col z-40',
+        'fixed top-0 right-0 h-screen w-[420px] bg-white flex flex-col z-40 outline-none focus:outline-none',
         className
       )}
       style={{
